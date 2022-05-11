b0192d11428fda3715348fa348c0850c375afa2d
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-05-11 23:19:56 +0200

    Added nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Cart } from "../assets/icon-cart.svg";
+import { ReactComponent as Close } from "../assets/icon-close.svg";
+import { ReactComponent as Menu } from "../assets/icon-menu.svg";
 import Avatar from "../assets/image-avatar.png";
 import { ReactComponent as Logo } from "../assets/logo.svg";
-import NavBarItems from "./NavBarItems";
 import "./NavBar.scss";
 
 export default function NavBar() {
+  const navBarItems = ["Collections", "Men", "Women", "About", "Contact"];
+  const [isMenuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!isMenuActive);
+  };
+
   return (
     <>
-      <div className="navbar-container">
-        <div className="logo-container">
-          <Logo />
-        </div>
+      <nav className="navbar-container">
+        <Menu className="navbar-container__menu-icon" onClick={toggleMenu} />
+        <div
+          className={
+            isMenuActive
+              ? "navbar-container__menu-overlay active"
+              : "navbar-container__menu-overlay"
+          }
+        />
 
-        <div className="item-container">
-          <NavBarItems />
+        <Logo className="navbar-container__logo" />
+
+        <div
+          className={
+            isMenuActive
+              ? "navbar-item-container active"
+              : "navbar-item-container"
+          }
+        >
+          <div className="close-container">
+            <Close
+              className="close-container__menu-close"
+              onClick={toggleMenu}
+            />
+          </div>
+
+          {navBarItems.map((item, index) => {
+            return (
+              <div key={index} className="navbar-item-container__item">
+                {item}
+              </div>
+            );
+          })}
         </div>
 
         <div className="right-container">
@@ -22,7 +56,7 @@ export default function NavBar() {
 
           <img src={Avatar} alt="Avatar" />
         </div>
-      </div>
+      </nav>
     </>
   );
 }
